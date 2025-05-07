b0363bf64243e13bebad4bd89fc1dfebc8bcc6c4
tyaaralestari <tyraalestari2004@gmail.com>
2025-05-07 12:01:48 +0800

    saya pintar

--- menu.cpp
+++ menu.cpp
@@ -2,14 +2,19 @@
 #include <conio.h>
 using namespace std;
 
-
 void info(){
+  system("cls");
+  cout<<"Indonesia hebat, poliban juara";
+  getch();
+}
+
+void dMenu(){
 system("cls");
 cout<<"Aplikasi Tampilan Menu"<<"\n";       
 cout<<"1. Menu Pertama"<<"\n";            
 cout<<"2. Menu Kedua"<<"\n";            
 cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
+cout<<"4. info"<<"\n";            
 cout<<"5. Exit"<<"\n";           
 cout<<"Masukan angka :";        
 
@@ -43,7 +48,7 @@ do
     /* code */
     break;  
    case '4':
-    mPertama("ke- empat");
+    info();
     /* code */
     break;  
   case '5':
